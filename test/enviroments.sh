006c9ea976c8b57851657fc13dc722be5abc0271
Marcos Nils <marcosnils@gmail.com>
2014-05-04 02:31:11 -0300

    Remove unused variables and files

--- test/enviroments.sh
+++ test/enviroments.sh
@@ -7,10 +7,4 @@ else
 	NGINX=$(which nginx)
 fi
 
-DOMAIN=api.mercadolibre.com
 DOMAIN_PORT=8080
-UPSTREAM=internal.mercadolibre.com.priv.net
-METHODS=( GET POST PUT DELETE OPTIONS )
-AB_COMMAND=$(which ab)
-HTTPERF_COMMAND=$(which httperf)
-HTTPERF_TMP_URLS="./httperf_casessh_urls"
