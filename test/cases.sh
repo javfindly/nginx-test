1f41425eedb4d38cbc70bbda7851cd90014e2da2
Marcos Nils <marcosnils@gmail.com>
2014-08-04 16:55:59 -0300

    Se agregan comentarios y un ejemplo por headers

--- test/cases.sh
+++ test/cases.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -x
 ##  METHOD DOMAIN RESOURCE EXPECTED_UPSTREAM
 
+#Multiple domains
 test_rule all_fe [www.mydomain.com,mydomain.com] "/" main-upstream
+
+#Someurl
 test_rule all_fe www.mydomain.com "/someurl" someurl-upstream
 
+#Different domains
 test_rule all_fe mobile.mydomain.com "/" mobile-upstream
 
+#Specific methods
 test_rule POST postonly.mydomain.com "/" main-upstream
+
+#Test headers
+test_rule GET www.mydomain.com "/" main-cookie-upstream "Cookie:'test=cookie'"
